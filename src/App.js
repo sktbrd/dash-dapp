4659081d87ced159df7dcbdf08069214f8763b4b
highlander <bithighlander@gmail.com>
2022-12-31 16:38:28 -0700

    sending DASH

--- src/App.js
+++ src/App.js
@@ -31,170 +31,225 @@ let {
 const pioneerApi = require("@pioneer-platform/pioneer-client")
 const coinSelect = require('coinselect')
 
+let spec = 'http://localhost:1646/spec/swagger.json'
+let configKeepKey = {
+  pairingInfo:{
+    name: process.env['SERVICE_NAME'] || 'DASH',
+    imageUrl: process.env['SERVICE_IMAGE_URL'] || 'https://assets.coincap.io/assets/icons/dash@2x.png',
+    basePath:spec
+  }
+}
+
+const configPioneer = {
+  queryKey:'sdk:test-tutorial-medium',
+  username:"dash-dapp",
+  spec:"https://pioneers.dev/spec/swagger.json"
+  // spec:"http://localhost:9001/spec/swagger.json"
+}
+
 function App() {
   const [address, setAddress] = useState('')
   const [balance, setBalance] = useState('0.000')
   const [amount, setAmount] = useState('0.00000000')
   const [toAddress, setToAddress] = useState('')
+  const [txid, setTxid] = useState(null)
   const [inputs, setInputs] = useState([])
-  const [pioneer, setPioneer] = useState("")
-  const [error, setError] = useState("")
-
+  const [signedTx, setSignedTx] = useState(null)
+  const [error, setError] = useState(null)
   const { isOpen, onOpen, onClose } = useDisclosure()
 
   let onSend = async function(){
     try{
-      // const configPioneer = {
-      //   queryKey:'sdk:test-tutorial-medium',
-      //   username:"dash-dapp",
-      //   //spec:"https://pioneers.dev/spec/swagger.json"
-      //   spec:"http://localhost:9001/spec/swagger.json"
-      // }
-      // let pioneer = new pioneerApi(configPioneer.spec,configPioneer)
-      // pioneer = await pioneer.init()
+
+      let pioneer = new pioneerApi(configPioneer.spec,configPioneer)
+      pioneer = await pioneer.init()
+
+      //init
+      let sdk = await KeepKeySdk.create(configKeepKey)
+      localStorage.setItem("apiKey",configKeepKey.apiKey);
+      console.log("config: ",configKeepKey.apiKey)
+
+      //balance check
+      let amountOut = parseInt(amount * 100000000)
+      console.log("amountOut: ",amountOut)
+      let balanceSat = balance * 100000000
+      if(amountOut >  balanceSat) throw new Error("Insufficient funds!")
+
+      //prepare coinselect
+      let utxos = []
+      for(let i = 0; i < inputs.length; i++){
+        let input = inputs[i]
+        let utxo = {
+          txId:input.txid,
+          vout:input.vout,
+          value:parseInt(input.value),
+          nonWitnessUtxo: Buffer.from(input.hex, 'hex'),
+          hex: input.hex,
+          tx: input.tx,
+          path:input.path
+        }
+        utxos.push(utxo)
+      }
+      if (utxos.length === 0) throw Error("101 YOUR BROKE! no UTXO's found! ")
+
+      let outputs = [
+        {
+          address:toAddress,
+          value: amountOut
+        }
+      ]
+
+      //@TODO dont use flag fee
+      // let feeRateInfo = await pioneer.GetFeeInfo({coin:"DASH"})
+      // feeRateInfo = feeRateInfo.data
+      // console.log("feeRateInfo: ",feeRateInfo)
+      let feeRateInfo = 10
+
+      //coinselect
+      console.log("input coinSelect: ",{utxos, outputs, feeRateInfo})
+      let selectedResults = coinSelect(utxos, outputs, feeRateInfo)
+      console.log("result coinselect algo: ",selectedResults)
+
       //
-      // //
-      // console.log("Sending Dash!")
-      // console.log("inputs: ",inputs)
-      // console.log("toAddress: ",toAddress)
-      // console.log("amount: ",amount)
-      //
-      // //balance check
-      // let amountOut = parseInt(amount * 100000000)
-      // console.log("amountOut: ",amountOut)
-      // let balanceSat = balance * 100000000
-      // if(amountOut >  balanceSat) throw new Error("Insufficient funds!")
-      //
-      // //prepare coinselect
-      // let utxos = []
-      // for(let i = 0; i < inputs.length; i++){
-      //   let input = inputs[i]
-      //   let utxo = {
-      //     txId:input.txid,
-      //     vout:input.vout,
-      //     value:parseInt(input.value),
-      //     nonWitnessUtxo: Buffer.from(input.hex, 'hex'),
-      //     hex: input.hex,
-      //     tx: input.tx,
-      //     path:input.path
-      //   }
-      //   utxos.push(utxo)
-      // }
-      // if (utxos.length === 0) throw Error("101 YOUR BROKE! no UTXO's found! ")
-      //
-      // let outputs = [
-      //   {
-      //     address:toAddress,
-      //     value: amountOut
-      //   }
-      // ]
-      //
-      // // let feeRateInfo = await pioneer.GetFeeInfo({coin:"DASH"})
-      // // feeRateInfo = feeRateInfo.data
-      // // console.log("feeRateInfo: ",feeRateInfo)
-      // let feeRateInfo = 10
-      //
-      // //coinselect
-      // console.log("input coinSelect: ",{utxos, outputs, feeRateInfo})
-      // let selectedResults = coinSelect(utxos, outputs, feeRateInfo)
-      // console.log("result coinselect algo: ",selectedResults)
-      //
-      // //
-      // let inputs = []
-      // for(let i = 0; i < selectedResults.inputs.length; i++){
-      //   //get input info
-      //   let inputInfo = selectedResults.inputs[i]
-      //   console.log("inputInfo: ",inputInfo)
-      //   let input = {
-      //     addressNList:bip32ToAddressNList(inputInfo.path),
-      //     scriptType:core.BTCInputScriptType.SpendWitness,
-      //     amount:String(inputInfo.value),
-      //     vout:inputInfo.vout,
-      //     txid:inputInfo.txId,
-      //     segwit:false,
-      //     hex:inputInfo.hex,
-      //     tx:inputInfo.tx
-      //   }
-      //   inputs.push(input)
-      // }
-      //
-      // const outputsFinal = []
-      // console.log("selectedResults.outputs: ",selectedResults.outputs)
-      // console.log("outputsFinal: ",outputsFinal)
-      // for(let i = 0; i < selectedResults.outputs.length; i++){
-      //   let outputInfo = selectedResults.outputs[i]
-      //   console.log("outputInfo: ",outputInfo)
-      //   if(outputInfo.address){
-      //     //not change
-      //     let output = {
-      //       address:toAddress,
-      //       addressType:"spend",
-      //       scriptType:core.BTCInputScriptType.SpendWitness,
-      //       amount:String(outputInfo.value),
-      //       isChange: false,
-      //     }
-      //     if(output.address)outputsFinal.push(output)
-      //   } else {
-      //     //change
-      //     let output = {
-      //       address:changeAddress,
-      //       addressType:"spend",
-      //       scriptType:core.BTCInputScriptType.SpendWitness,
-      //       amount:String(outputInfo.value),
-      //       isChange: true,
-      //     }
-      //     if(output.address)outputsFinal.push(output)
-      //   }
-      //   console.log(i,"outputsFinal: ",outputsFinal)
-      // }
-      //
-      // let hdwalletTxDescription = {
-      //   coin: 'Bitcoin',
-      //   inputs,
-      //   outputs:outputsFinal,
-      //   version: 1,
-      //   locktime: 0,
-      // }
-      //
-      // //signTx
-      // console.log("**** hdwalletTxDescription: ",hdwalletTxDescription)
-      // let signedTxTransfer = await window['wallet'].btcSignTx(hdwalletTxDescription)
-      // console.log("signedTxTransfer: ",signedTxTransfer)
-      //
-      //
-      // //broadcast TX
-      // let broadcastBodyTransfer = {
-      //   network:"BTC",
-      //   serialized:signedTxTransfer.serializedTx,
-      //   txid:"unknown",
-      //   invocationId:"unknown"
-      // }
-      // let resultBroadcastTransfer = await pioneer.instance.Broadcast(null,broadcastBodyTransfer)
-      // console.log("resultBroadcast: ",resultBroadcastTransfer)
+      let inputsSelected = []
+      for(let i = 0; i < selectedResults.inputs.length; i++){
+        //get input info
+        let inputInfo = selectedResults.inputs[i]
+        console.log("inputInfo: ",inputInfo)
+        let input = {
+          addressNList:bip32ToAddressNList(inputInfo.path),
+          scriptType:core.BTCInputScriptType.SpendAddress,
+          amount:String(inputInfo.value),
+          vout:inputInfo.vout,
+          txid:inputInfo.txId,
+          segwit:true,
+          hex:inputInfo.hex
+        }
+        inputsSelected.push(input)
+      }
+
+      //get xpub for change addresses
+      let path =
+        {
+          symbol: 'DASH',
+          address_n: [0x80000000 + 44, 0x80000000 + 5, 0x80000000 + 0],
+          coin: 'Bitcoin',
+          script_type: 'p2pkh',
+          showDisplay: false
+        }
+
+      let responsePubkeyChange = await sdk.system.info.getPublicKey(path)
+      console.log("responsePubkeyChange: ", responsePubkeyChange)
+      console.log("responsePubkeyChange: ", responsePubkeyChange.xpub)
+
+      //get change address
+      let changeAddyIndex = await pioneer.GetChangeAddress({network:'DASH',xpub:responsePubkeyChange.xpub})
+      changeAddyIndex = changeAddyIndex.data.changeIndex
+      console.log("changeAddyIndex: ",changeAddyIndex)
+
+      let addressInfo = {
+        addressNList: [0x80000000 + 44, 0x80000000 + 5, 0x80000000 + 0, 0, changeAddyIndex],
+        coin: 'Dash',
+        scriptType: 'p2pkh',
+        showDisplay: false
+      }
+
+      let changeAddress = await sdk.address.uTXOGetAddress({
+        address_n: addressInfo.addressNList,
+        script_type:addressInfo.scriptType,
+        coin:addressInfo.coin
+      })
+      changeAddress = changeAddress.address
+      console.log("changeAddress: ",changeAddress)
+
+      const outputsFinal = []
+      console.log("selectedResults.outputs: ",selectedResults.outputs)
+      console.log("outputsFinal: ",outputsFinal)
+      for(let i = 0; i < selectedResults.outputs.length; i++){
+        let outputInfo = selectedResults.outputs[i]
+        console.log("outputInfo: ",outputInfo)
+        if(outputInfo.address){
+          //not change
+          let output = {
+            address:toAddress,
+            addressType:"spend",
+            scriptType:core.BTCInputScriptType.SpendAddress,
+            amount:String(outputInfo.value),
+            isChange: false,
+          }
+          if(output.address)outputsFinal.push(output)
+        } else {
+          //change
+          let output = {
+            addressNList: addressInfo.addressNList,
+            addressType:"spend",
+            scriptType:core.BTCInputScriptType.SpendAddress,
+            amount:String(outputInfo.value),
+            isChange: true,
+          }
+          if(output.addressNList)outputsFinal.push(output)
+        }
+        console.log(i,"outputsFinal: ",outputsFinal)
+      }
+
+
+      let hdwalletTxDescription = {
+        coin: 'Dash',
+        inputs:inputsSelected,
+        outputs:outputsFinal,
+        version: 1,
+        locktime: 0,
+      }
+
+      //signTx
+      let signedTxTransfer = await sdk.bitcoin.btcSignTransaction(hdwalletTxDescription)
+      signedTxTransfer = JSON.parse(signedTxTransfer)
+      setSignedTx(signedTxTransfer.serializedTx)
 
     }catch(e){
-      console.error(e)
-      setError(e.message)
+      console.error("Error on send!",e)
+    }
+  }
+
+  let onBroadcast = async function(){
+    let tag = " | onBroadcast | "
+    try{
+      console.log("onBroadcast: ",onBroadcast)
+      let pioneer = new pioneerApi(configPioneer.spec,configPioneer)
+      pioneer = await pioneer.init()
+
+      //broadcast TX
+      let broadcastBodyTransfer = {
+        network:"DASH",
+        serialized:signedTx,
+        txid:"unknown",
+        invocationId:"unknown"
+      }
+      let resultBroadcastTransfer = await pioneer.Broadcast(null,broadcastBodyTransfer)
+      resultBroadcastTransfer = resultBroadcastTransfer.data
+      console.log("resultBroadcastTransfer: ",resultBroadcastTransfer)
+      // resultBroadcastTransfer = resultBroadcastTransfer.data
+      if(resultBroadcastTransfer.error){
+        setError(resultBroadcastTransfer.error)
+      }
+      if(resultBroadcastTransfer.txid){
+        setTxid(resultBroadcastTransfer.txid)
+      }
+    }catch(e){
+      console.error(tag,e)
     }
   }
 
   let onStart = async function(){
     try{
-      let spec = 'http://localhost:1646/spec/swagger.json'
+
       let apiKey = localStorage.getItem("apiKey");
-      let config = {
-        apiKey: apiKey || 'test-123',
-        pairingInfo:{
-          name: process.env['SERVICE_NAME'] || 'DASH',
-          imageUrl: process.env['SERVICE_IMAGE_URL'] || 'https://assets.coincap.io/assets/icons/dash@2x.png',
-          basePath:spec
-        }
-      }
+      configKeepKey.apiKey = apiKey
 
       //init
-      let sdk = await KeepKeySdk.create(config)
-      localStorage.setItem("apiKey",config.apiKey);
-      console.log("config: ",config.apiKey)
+      let sdk = await KeepKeySdk.create(configKeepKey)
+      localStorage.setItem("apiKey",configKeepKey.apiKey);
+      console.log("config: ",configKeepKey.apiKey)
 
       let path =
         {
@@ -209,15 +264,8 @@ function App() {
       console.log("responsePubkey: ", responsePubkey)
       console.log("responsePubkey: ", responsePubkey.xpub)
 
-      const configPioneer = {
-        queryKey:'sdk:test-tutorial-medium',
-        username:"dash-dapp",
-        //spec:"https://pioneers.dev/spec/swagger.json"
-        spec:"http://localhost:9001/spec/swagger.json"
-      }
       let pioneer = new pioneerApi(configPioneer.spec,configPioneer)
       pioneer = await pioneer.init()
-      setPioneer(pioneer)
 
       //get balance DASH
       let data = await pioneer.ListUnspent({network:'DASH',xpub:responsePubkey.xpub})
@@ -235,10 +283,12 @@ function App() {
 
       //get new address
       let newAddyIndex = await pioneer.GetChangeAddress({network:'DASH',xpub:responsePubkey.xpub})
+      newAddyIndex = newAddyIndex.data.changeIndex
+      newAddyIndex = newAddyIndex + 1
       console.log("newAddyIndex: ",newAddyIndex)
 
       let addressInfo = {
-        addressNList: [0x80000000 + 44, 0x80000000 + 5, 0x80000000 + 0, 0x80000000 + 0],
+        addressNList: [0x80000000 + 44, 0x80000000 + 5, 0x80000000 + 0, 0, newAddyIndex],
         coin: 'Dash',
         scriptType: 'p2pkh',
         showDisplay: false
@@ -259,7 +309,7 @@ function App() {
   // onStart()
   useEffect(() => {
     onStart()
-  }, [])
+  }, []) //once on startup
 
   const handleInputChangeAmount = (e) => {
     const inputValue = e.target.value;
@@ -294,13 +344,28 @@ function App() {
                               onChange={handleInputChangeAddress}
             />
             </div>
-            <small>Donate DASH to developer: (' XwNbd46qdmbVWLdXievBhBMW7JYy8WiE7n ')</small>
+            <br/>
+            {error ? <div>error: {error}</div> : <div></div>}
+            {txid ? <div>txid: {txid}</div> : <div></div>}
+            {txid ? <div></div> : <div>
+              {signedTx ? <div>signedTx: {signedTx}</div> : <div></div>}
+            </div>}
+
           </ModalBody>
 
           <ModalFooter>
-            <Button colorScheme='green' mr={3} onClick={onSend}>
-              Send
-            </Button>
+            {!signedTx ? <div>
+              <Button colorScheme='green' mr={3} onClick={onSend}>
+                Send
+              </Button>
+            </div> : <div></div>}
+            {!txid ? <div>
+              {signedTx ? <div>
+                <Button colorScheme='green' mr={3} onClick={onBroadcast}>
+                  broadcast
+                </Button>
+              </div> : <div></div>}
+            </div> : <div></div>}
             <Button colorScheme='blue' mr={3} onClick={onClose}>
               exit
             </Button>
