9e2799841e9071f0581d5d567655f0a2368b48cf
Vlad 2 <116202536+sktbrd@users.noreply.github.com>
2023-01-10 18:34:29 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -358,7 +358,7 @@ function App() {
             </div>}
 
           </ModalBody>
-          Vlads Dash App
+          
           <ModalFooter>
             {!signedTx ? <div>
               <Button colorScheme='green' mr={3} onClick={onSend}>
@@ -392,7 +392,7 @@ function App() {
               <Text>
                 balance: {balance}
               </Text>
-              <Button onClick={onOpen}>Send Dash</Button>
+              <Button onClick={onOpen}>Send Freaking Dash</Button>
             </div>}
           </VStack>
         </Grid>
